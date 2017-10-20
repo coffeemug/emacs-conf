e62db25d872ba9d7b0fd09cf13552657a71590ef
Slava Akhmechet <slava@stripe.com>
2017-10-19 17:27:51 -0700

    enhancing burndown

--- burndown-mode.el
+++ burndown-mode.el
@@ -3,33 +3,61 @@
 (provide 'burndown-mode)
 
 ;; define our mode-line expression
-(setq burndown-mode-mode-line-expr '(:eval (paragraph-burndown-modeline-str)))
+(setq burndown-mode-mode-line-expr '(:eval (burndown-str)))
 
-;; twitter-style burndown -- count down from 140 chars/peragraph (+
-;; how many 140 char sentences are written)
-(defun paragraph-burndown-modeline-str ()
-  (let* ((beg (if (use-region-p)
-		  (point)
-		(save-excursion
-		  (move-to-left-margin)
-		  (forward-paragraph -1)
-		  (point))))
-	 (end (if (use-region-p)
-		  (mark)
-		(save-excursion
-		  (move-to-left-margin)
-		  (forward-paragraph +1)
-		  (point))))
-	 (pwidth (string-width (buffer-substring-no-properties beg end)))
-	 (ntweets (/ pwidth 140))
-	 (nchars (- 140 (% pwidth 140))))
-    (if (zerop (string-width (thing-at-point 'line t)))
-	" 0x/140"
+;; twitter-style burndown -- count down from 140 chars/paragraph (+
+;; how many 140 char sentences are written total)
+(defun burndown-char-str ()
+  (let* ((char-cutoff 140)
+	 bbeg bend pbeg pend)
+    (if (use-region-p)
+	(setq bbeg (point)
+	      bend (mark)
+	      pbeg bbeg
+	      pend bend)
+      (setq bbeg (point-min)
+	    bend (point-max)
+	    pbeg (save-excursion
+		   (move-to-left-margin)
+		   (forward-paragraph -1)
+		   (point))
+	    pend (save-excursion
+		   (move-to-left-margin)
+		   (forward-paragraph +1)
+		   (point))))
+    (let* ((pwidth (string-width (buffer-substring-no-properties pbeg pend)))
+	   (bwidth (string-width (buffer-substring-no-properties bbeg bend)))
+	   (ntweets (/ bwidth char-cutoff))
+	   (nchars (- char-cutoff (% pwidth char-cutoff))))
+      (if (or (= 0 (buffer-size))
+	      (zerop (string-width (thing-at-point 'line t))))
+	  (setq nchars 140))
       (concat
-       (format " %dx/" ntweets)
+       " c:"
        (if (<= nchars 20)
 	   (propertize (format "%s" nchars) 'face 'warning)
-	 (format "%d" nchars))))))
+	 (format "%d" nchars))
+       (format "/%dx" ntweets)))))
+
+;; count down buffer word burndown (from 500 + how many 500 words are
+;; written)
+(defun burndown-word-str ()
+  (let* ((word-cutoff 250)
+	 (beg (point-min))
+	 (end (point-max)))
+    (when (use-region-p)
+      (setq beg (point)
+	    end (mark)))
+    (let* ((nwords (count-words beg end))
+	   (nsections (/ nwords word-cutoff))
+	   (nburndown (- word-cutoff (% nwords word-cutoff))))
+      (format " w:%d/%dx" nburndown nsections))))
+
+;; Get it all together
+(defun burndown-str ()
+  (concat
+   (burndown-char-str)
+   (burndown-word-str)))
 
 ;; setup/cleanup
 (defun setup-burndown-mode ()
